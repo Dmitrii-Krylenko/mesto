3887ab968672b8a56981b19d520da55081382c52
Dmitrii-Krylenko <120057236+Dmitrii-Krylenko@users.noreply.github.com>
2023-02-17 20:47:25 +0300

    cssFix

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 let editProfile = document.querySelector('.profile__edit');
-let editPopup = document.querySelector('.popup_display-none');
+let editPopup = document.querySelector('.popup_opened');
 function open () {
-    editPopup.classList.remove('popup_display-none');
+    editPopup.classList.add('popup_opened');
 }
 function close () {
-    editPopup.classList.add('popup_display-none');
+    editPopup.classList.remove('popup_opened');
 }
 editProfile.addEventListener('click', open);
 
@@ -16,8 +16,8 @@ let userOccupation = 'Исследователь океана';
 
 let nameWrite = document.querySelector('.profile__name');
 let occupationWrite = document.querySelector('.profile__occupation');
-let nameValue = document.querySelector('.popup__input_name');
-let occupationValue = document.querySelector('.popup__input_occupation');
+let nameValue = document.querySelector('.popup__input_inter_name');
+let occupationValue = document.querySelector('.popup__input_inter_occupation');
 nameWrite.textContent = userName;
 nameValue.value = userName;
 occupationValue.value = userOccupation;
