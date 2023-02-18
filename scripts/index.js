08ddde900f90fffaeb55e1834696154099b596df
Dmitrii-Krylenko <120057236+Dmitrii-Krylenko@users.noreply.github.com>
2023-02-18 19:43:05 +0300

    JSfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,38 @@
 let editProfile = document.querySelector('.profile__edit');
-let editPopup = document.querySelector('.popup_opened');
-function open () {
+let editPopup = document.querySelector('.popup');
+function open() {
     editPopup.classList.add('popup_opened');
+    nameValue.value = nameWrite.textContent;
+    occupationValue.value = occupationWrite.textContent;
 }
-function close () {
+console.log(open)
+function close() {
     editPopup.classList.remove('popup_opened');
 }
 editProfile.addEventListener('click', open);
 
-let closePopup = document.querySelector('.popup__close ');
+let closePopup = document.querySelector('.popup__close');
 closePopup.addEventListener('click', close);
 
-let userName = 'Жак-Ив Кусто';
-let userOccupation = 'Исследователь океана';
+// let userName = 'Жак-Ив Кусто';
+// let userOccupation = 'Исследователь океана';
 
 let nameWrite = document.querySelector('.profile__name');
 let occupationWrite = document.querySelector('.profile__occupation');
 let nameValue = document.querySelector('.popup__input_inter_name');
 let occupationValue = document.querySelector('.popup__input_inter_occupation');
-nameWrite.textContent = userName;
-nameValue.value = userName;
-occupationValue.value = userOccupation;
-occupationWrite.textContent = userOccupation;
+// nameWrite.textContent = userName;
+// nameValue.value = nameWrite.textContent;
+// occupationValue.value = occupationWrite.textContent;
+// occupationWrite.textContent = userOccupation;
 
 let formElement = document.querySelector('.popup__form');
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
     evt.preventDefault();
-nameWrite.textContent = nameValue.value;
-occupationWrite.textContent =occupationValue.value;
-close ();
+    nameWrite.textContent = nameValue.value;
+    occupationWrite.textContent = occupationValue.value;
+    close();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
